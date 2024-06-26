b1a18375576d37fc6c4aa87a9fdbd5de117aefc3
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-06-26 14:40:56 +0200

    Readme updated, and all routing done

--- api/server.js
+++ api/server.js
@@ -3,28 +3,13 @@
 // BREAK UP THIS MONOLITHIC FILE USING ROUTES
 const express = require('express');
 const adaptersRouter = require('./adopters/adopters-router')
+const dogsrouter = require('./dogs/dogs-router')
 
 const server = express();
 
 server.use(express.json());
-server.use(adaptersRouter)
-
-const Dog = require('./dogs/dogs-model');
-
-
-// DOGS ENDPOINTS
-// DOGS ENDPOINTS
-// DOGS ENDPOINTS
-server.get('/api/dogs', (req, res) => {
-  Dog.find()
-    .then(dogs => {
-      res.status(200).json(dogs);
-    })
-    .catch(error => {
-      console.log(error);
-      res.status(500).json({ message: 'Error retrieving the dogs' });
-    });
-});
+server.use('/api/adopters',adaptersRouter)
+server.use('/api/dogs', dogsrouter)
 
 // OTHER ENDPOINTS
 // OTHER ENDPOINTS
